f3d7daf408f19bbdbcf86a2aa4e2b5e1acc0be55
Shahbaz Khan <khan.shahbaz4595@gmail.com>
2024-01-28 16:52:57 +0100

    Feat: Added footer component

---
+++ src/components/Footer.tsx
@@ -0,0 +1,37 @@
+import Link from 'next/link';
+
+type FooterItem = {
+  id: number;
+  text: string;
+};
+
+export const Footer = () => {
+  const footer = {
+    footerText: `© ${new Date().getFullYear()} Acme Co. All rights reserved.`,
+    footerItems: [
+      { id: 1, text: 'Terms' },
+      { id: 2, text: 'Privacy' },
+      { id: 3, text: 'Cookies' },
+    ] as FooterItem[],
+  };
+
+  return (
+    <div className='px-8'>
+      <footer className='md:px-8'>
+        <hr />
+        <div className='py-8 flex flex-col-reverse justify-center items-center md:flex-row gap-8 '>
+          <p className='text-base grow text-center md:text-start'>
+            {footer.footerText}
+          </p>
+          <ul className='text-base flex gap-4 items-start md:items-center'>
+            {footer.footerItems.map((footerItem) => (
+              <li key={footerItem.id}>
+                <Link href='#'>{footerItem.text}</Link>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </footer>
+    </div>
+  );
+};
